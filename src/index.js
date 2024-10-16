7d69420d1be2f45f820bfa48807b46735a1f6bac
Anna Anello <125416908+Ollena5@users.noreply.github.com>
2024-10-16 21:02:30 +0000

    got react to work on my end, needed to install npm and react-scripts

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Optional if you have a CSS file
+import App from './App'; // Ensure you have App.js or change the path to your main component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
